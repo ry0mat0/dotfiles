a09590fd8bd25bbb2d6e476f483702f0f549cfc4
Matoike <matoike.ryota@qst.go.jp>
2023-04-13 13:43:57 +0900

    feat: use ime and disable audible_bell

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,9 +13,11 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+config.use_ime = true
 config.color_scheme = 'iceberg-dark'
 config.font = wezterm.font('HackGen Console NF')
 config.font_size = 14.0
+config.audible_bell = 'Disabled'
 
 -- and finally, return the configuration to wezterm
 return config
